230202c7b79758dd46eff728fa98088a75012b3d
bizsys-takeuchisho <takeuchisho@nttdata-bizsys.co.jp>
2024-08-23 09:22:18 +0900

    ファイル整理

--- sql/QC_Deploy_test_1.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Worker1
-(worker_id    CHAR(5)       NOT NULL,
- name         VARCHAR(20)   NOT NULL,
- birthday     DATE          NOT NULL,
- gender       INTEGER       NOT NULL,
- prefecture   VARCHAR(50)   NOT NULL,
- hire_date    DATE          NOT NULL,
- department   INTEGER       NOT NULL,
- PRIMARY KEY (worker_id));
